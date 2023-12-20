d7fdf24b652c365976565625f04821888bcad635
01187584 <01187584@pw.edu.pl>
2023-12-20 23:39:38 +0100

    teraz już nie dzielimy przez 0; gdy zwracamy 1, to macierz może, ale nie musi być osobliwa

--- gauss.c
+++ gauss.c
@@ -3,7 +3,7 @@
 
 /**
  * Zwraca 0 - eliminacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
+ * Zwraca 1 - macierz moze byc osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
     int i, j, k;
@@ -12,8 +12,8 @@ int eliminate(Matrix *mat, Matrix *b){
     {
         for(j=i+1; j<mat->r; j++)
         {
-            mnoznik = mat->data[j][i] / mat->data[i][i];
             if (mat->data[i][i]==0) return 1;
+            mnoznik = mat->data[j][i] / mat->data[i][i];
             for(k=0; k<mat->c; k++)
             {
                 mat->data[j][k] -= mnoznik * mat->data[i][k];
@@ -21,7 +21,7 @@ int eliminate(Matrix *mat, Matrix *b){
             b->data[j][0] -= mnoznik * b->data[i][0];
         }
     }
-    if (mat->data[i][i]==0) return 1;
+    if (mat->data[0][0]==0) return 1;
     return 0;
 }
 
