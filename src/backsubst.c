a6f7ec4ea3030d0519c1650f2dd072c843381239
01187584 <01187584@pw.edu.pl>
2023-12-22 11:15:46 +0100

    Sprawdzenie czy dzielimy przez około zero w backsubst.c

--- src/backsubst.c
+++ src/backsubst.c
@@ -1,4 +1,6 @@
 #include "backsubst.h"
+#include <math.h>
+#define ZERO 0.000001
 /**
  * Zwraca 0 - wsteczne podstawienie zakonczone sukcesem
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
@@ -8,7 +10,7 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 				int i,j;
 				if (mat->c != b->r) return 2;
 				for (i = mat->r-1; i >= 0; i--) {
-					if (mat->data[i][i] == 0) return 1;
+					if (fabs(mat->data[i][i]) < ZERO) return 1;
 					b->data[i][0] *= (1/mat->data[i][i]);
 					mat->data[i][i] = 1.0;
 					for (j = i-1; j >= 0; j--) {
@@ -16,7 +18,7 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 						mat->data[j][i] = 0.0;
 					}
 				}
-				for (i =0; i < x->r; i++) {
+				for (i = 0; i < x->r; i++) {
 					x->data[i][0] = b->data[i][0];
 				}
 
